af6f8149608f4ec326f50addf8a66fdec745674c
LUIZ HENRIQUE PAROLIM DOMINGUES <u21248@academico.cotuca.unicamp.br>
2022-10-01 11:10:39 -0300

    Iniciando projeto

---
+++ 21248, 21251 - Projeto1_TOO.c
@@ -0,0 +1,8 @@
+// Luiz Henrique Parolim Domingues - 21248,
+// Matheus Henrique De Oliveira Freire - 21251.
+
+// Projeto 1 - Tópicos em Orientação a objetos;
+// Linguagem C;
+// Projeto de solução de sistemas lineares
+// A partir do método de Gauss-Seidel.
+

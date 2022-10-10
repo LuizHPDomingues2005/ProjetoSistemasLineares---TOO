931df9eaf5fb0bb03f199cce9816ee921f67b82f
MatheusFreire7 <105430711+MatheusFreire7@users.noreply.github.com>
2022-10-10 10:58:28 -0300

    10/10

--- gauss-seidel/matriz_Gauss.c
+++ gauss-seidel/matriz_Gauss.c
@@ -3,60 +3,63 @@
 
 int i,j,k,ordem;
 float matriz[20][20],c,x[10],soma=0.0;
-    
-    int ehAplicavel() // testar se e possivel resolver o sistema dividindo a linha por                     outra linha
+
+    int ehAplicavel() // testar se e possivel resolver o sistema dividindo a linha por outra linha
     {
          float divisaoLinhas[ordem];
          float resultadoDivisao[ordem];
          memset(divisaoLinhas, 0, sizeof(divisaoLinhas));
          memset(resultadoDivisao, 0, sizeof(resultadoDivisao));
-         for (int linha = 1; linha <= ordem; linha++) 
+         for (int linha = 0; linha <= ordem; linha++) //percorre a linha da matriz
          {
-            for (int coluna = 1; coluna <= ordem+1; coluna++) 
+            for (int coluna = 1; coluna <= ordem+1; coluna++) // percorre a coluna da matriz da linha atual
             {
-                divisaoLinhas[coluna] = matriz[linha][coluna];
+                divisaoLinhas[coluna] = matriz[linha][coluna];  // vetor recebe os dados da linha que estamos percorrendo
             }
-            
-              for (int coluna = 1; coluna <= ordem; coluna++) 
+            for(int linhaSeguinte = linha+1; linhaSeguinte <= ordem; linhaSeguinte++)
             {
-                if( divisaoLinhas[coluna] > matriz[linha+1][coluna])
-                    resultadoDivisao[coluna] = divisaoLinhas[coluna] / matriz[linha+1][coluna];
-                    
-                if( divisaoLinhas[coluna] < matriz[linha+1][coluna])
-                     resultadoDivisao[coluna] = matriz[linha+1][coluna] /  divisaoLinhas[coluna];
-                     
-               if( divisaoLinhas[coluna] == matriz[linha+1][coluna])
-                     resultadoDivisao[coluna] = divisaoLinhas[coluna] / matriz[linha+1][coluna];
-                     
-            }
-        
-        int elementoVetor;
-        int primeiroAcesso = 1;
-        int elementosIgual = 1;
-      
-       for (i = 1; i <= ordem; i++) 
-       {
-           if(primeiroAcesso == 1)
-           {
-               elementoVetor = resultadoDivisao[i];
-               primeiroAcesso = 0;
-           }
-           else
-           {
-               if( elementoVetor ==  resultadoDivisao[i])
+                for (int coluna = 1; coluna <= ordem; coluna++)  //percorre a coluna da matriz da linha atual
+                {
+                    if( divisaoLinhas[coluna] > matriz[linhaSeguinte][coluna]) //acrescentamos linha+1
+                        resultadoDivisao[coluna] = divisaoLinhas[coluna] / matriz[linhaSeguinte][coluna];
+
+                    if( divisaoLinhas[coluna] < matriz[linhaSeguinte][coluna])
+                         resultadoDivisao[coluna] = matriz[linhaSeguinte][coluna] /  divisaoLinhas[coluna];
+
+                   if( divisaoLinhas[coluna] == matriz[linhaSeguinte][coluna])
+                         resultadoDivisao[coluna] = divisaoLinhas[coluna] / matriz[linhaSeguinte][coluna];
+                }
+
+                int elementoVetor;
+                int primeiroAcesso = 1;
+                int elementosIgual = 1;
+
+               for (i = 0; i <= ordem; i++)
                {
-                     elementosIgual++;
-                     printf("a");
-               }
-                else
-                    return 1;
-           }
+                   if(primeiroAcesso == 1)
+                   {
+                       elementoVetor = resultadoDivisao[i];
+                       primeiroAcesso = 0;
+                   }
+                   else
+                   {
+                       if( elementoVetor ==  resultadoDivisao[i])
+                       {
+                             elementosIgual++;
+                       }
+                        else
+                            return 1;
+                   }
+                }
+                if(elementosIgual == ordem)
+                    return 0; // se a quantidade de elementos do vetor são iguais ao tamanho da 3linha todos são iguais
+            }
         }
-        if(elementosIgual == ordem)
-            return 0; // se a quantidade de elementos do vetor são iguais ao tamanho da            linha todos são iguais
-        }
+
     }
-    
+
+
+
      void trocarLinhas(int contLinhas) // trocar linhas da matriz
     {
        int aux;
@@ -66,10 +69,10 @@ float matriz[20][20],c,x[10],soma=0.0;
            matriz[contLinhas][j]= matriz[contLinhas+1][j];
            matriz[contLinhas+1][j]= aux;
        }
-       
+
     }
 int main() {
-  
+
     printf("\nEscreva a ordem da matriz: ");
     scanf("%d", &ordem);
     fflush(stdin);
@@ -83,13 +86,13 @@ int main() {
             fflush(stdin);
         }
     }
-    
+
     if(ehAplicavel() == 0)
     {
         printf("Não é possivel resolver o sistema!");
         return 0;
     }
-    
+
     if(matriz[1][1] == 0)
     {
         int contLinhas = 1; //contador de linhas para trocar as linhas de lugares para tirar o zero da diagonal
@@ -119,8 +122,8 @@ int main() {
             }
         }
     }
-    
-    
+
+
     for(i=1;i<=ordem;i++)
     {
         for(j=1;j<=ordem+1;j++)

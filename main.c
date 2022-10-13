6ad5f550e7a98368995d7108ac84421683dbb017
LuizHPDomingues2005 <luizhpdomingues2@gmail.com>
2022-10-12 21:51:51 -0300

    12.10

--- main.c
+++ main.c
@@ -18,56 +18,83 @@ enum{
 boolean;
 
 
-void main()
+void trocarLinhasDeLugar(int ordem, double matriz[ordem][ordem], double resultados[ordem]){
+    int i, j;
 
+    for(i = 0; i != ordem; i++)
+ {
+    if(matriz[i][i] == 0)
+    {
+        int contLinhas = 0; //contador quantas vezes as linhas foram trocadas
+
+            int aux, aux2;
+
+                if(i != ordem -1)
+                {
+                    for(j = 0; contLinhas < ordem; j++)
+                    {
+                        aux = matriz[i][j];
+                        aux2 = resultados[i];
+
+                        matriz[i][j]= matriz[i+1][j];
+                        resultados[i]= resultados[i+1];
+
+                        matriz[i+1][j]= aux;
+                        resultados[i+1] = aux2;
+
+                        contLinhas++;
+                    }
+
+                }
+
+                else
+                {
+                    for(j = 0; contLinhas < ordem; j++)
+                    {
+                        aux = matriz[i][j];
+                        aux2 = resultados[i];
+
+                        matriz[i][j]= matriz[i-1][j];
+                        resultados[i]= resultados[i-1];
+
+                        matriz[i-1][j]= aux;
+                        resultados[i-1] = aux2;
+
+                        contLinhas++;
+
+                    }
+                }
+
+    }
+        if(matriz[i][i] == 0)
+                    {
+                        printf("Não é possivel resolver o sistema!");
+                        break;
+                    }
+    }
+}
+
+void testePrint(int ordem, double matriz[ordem][ordem])
 {
-  FILE *arq;
-  char Linha[100];
-  int result, ordem,i ,j;
-  // Abre um arquivo TEXTO para LEITURA
-  arq = fopen("matriz.txt", "r");
-  if (arq == NULL)  // Se houve erro na abertura
-  {
-     printf("Problemas na abertura do arquivo\n");
-     return;
-  }
+    for(int i = 0; i != ordem; i++)
+    {
+        for(int j = 0; j != ordem; j++)
+        {
+            printf("%lf ", matriz[i][j]);
+        }
+        printf("\n");
+    }
 
-  fscanf(arq, "%d", &ordem);
+}
 
-  double matriz[ordem][ordem];
-  double resultados[ordem];
+boolean verificarPossibilidadeDeSolucao(int ordem, double matriz[ordem][ordem]){
+    int i, j;
 
-
-  for( i = 0; i != ordem; i++)
-  {
-
-
-
-      for( j = 0; j != ordem; j++)
-      {
-          fscanf(arq, "%lf ", &matriz[i][j]);
-
-      }
-
-      fscanf(arq, "%lf ", &resultados[i]);
-      /*fseek(arq, ordem, SEEK_CUR);*/
-
-  }
-
-  fclose(arq);
-
-      // verificar pares
-      // verificamos linha com linha
-      // primeiro a linha 1 com as demais linhas: 2, 3, 4...
-      // após isso a linha 2 com as demais linhas: 3, 4...
-
-
-  double linha[ordem];
+    double linha[ordem];
   double resultadoDivisao[ordem], resultAnt[ordem];; // vetor para armazenar resultados
   boolean ehAplicavel = true, valoresIguals[ordem];
 
 
-  ///////////////////////////////////////////////////////////
   for (i = 0; i < ordem-1; i++) // repeticao para fazer pares de linhas com execao da ultima
          {
             for ( j = 0; j != ordem; j++)
@@ -114,60 +141,66 @@ void main()
             }
 
             if(ehAplicavel == false)
-                break;
+                return false;
 
         }
 
          }
+         return true;
+}
+
+void main()
+
+{
+  FILE *arq;
+  char Linha[100];
+  int result, ordem,i ,j;
+  // Abre um arquivo TEXTO para LEITURA
+  arq = fopen("matriz3.txt", "r");
+  if (arq == NULL)  // Se houve erro na abertura
+  {
+     printf("Problemas na abertura do arquivo\n");
+     return;
+  }
+
+  fscanf(arq, "%d", &ordem);
+
+  double matriz[ordem][ordem];
+  double resultados[ordem];
+
+
+  for( i = 0; i != ordem; i++)
+  {
+
+
+
+      for( j = 0; j != ordem; j++)
+      {
+          fscanf(arq, "%lf ", &matriz[i][j]);
+
+      }
+
+      fscanf(arq, "%lf ", &resultados[i]);
+
+  }
+
+  fclose(arq);
+
+  testePrint(ordem, matriz);
+      // verificar pares
+      // verificamos linha com linha
+      // primeiro a linha 1 com as demais linhas: 2, 3, 4...
+      // após isso a linha 2 com as demais linhas: 3, 4...
+
+
+
+  ///////////////////////////////////////////////////////////
+ if(verificarPossibilidadeDeSolucao(ordem, matriz))
+    printf("E possivel resolver.");
+ else
+    printf("Não e possivel resolver.");
+
  ////////////////////////////////////////////////////////////
 
- for(i = 0; i != ordem; i++)
- {
-    if(matriz[i][i] == 0)
-    {
-        int contLinhas = 0; //contador quantas vezes as linhas foram trocadas
-
-            int aux, aux2;
-
-                if(i != ordem -1)
-                {
-                    for(j = 0; contLinhas < ordem-1; j++)
-            {
-                    aux = matriz[i][j];
-                    aux2 = resultados[i];
-
-                    matriz[i][j]= matriz[i+1][j];
-                    resultados[i]= resultados[i+1];
-
-                    matriz[i+1][j]= aux;
-                    resultados[i+1] = aux2;
-
-                    contLinhas++;
-            }
-                }
-
-                else
-                {
-                    for(j = 0; contLinhas < ordem-1; j++)
-            {
-                    aux = matriz[i][j];
-                    aux2 = resultados[i];
-
-                    matriz[i][j]= matriz[i-1][j];
-                    resultados[i]= resultados[i-1];
-
-                    matriz[i-1][j]= aux;
-                    resultados[i-1] = aux2;
-
-                    contLinhas++;
-            }
-                }
-        if(contLinhas == ordem) // teste
-        {
-             printf("Não é possivel resolver o sistema!");
-        }
-    }
-
-    }
-
- }
+ trocarLinhasDeLugar(ordem, matriz, resultados);
+}
